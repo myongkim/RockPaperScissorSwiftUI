76d693738aa4783d5dbcd241d7e8db3ef0dfc7df
Isaac Kim <myong328@gmail.com>
2022-04-05 09:42:53 -0700

    Initial Commit

---
+++ RockPaperScissorSwiftUI/RockPaperScissorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorSwiftUIApp.swift
+//  RockPaperScissorSwiftUI
+//
+//  Created by Isaac Kim on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
